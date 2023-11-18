7de4d8640d0f2547ce1732452dd407036655c2e2
aoomUwU <119989552+aoomUwU@users.noreply.github.com>
2023-11-18 22:10:39 +0700

    Update converter.gd

--- Scripts/converter.gd
+++ Scripts/converter.gd
@@ -49,4 +49,14 @@ static func ConverBase16To2Base(arg: String) -> String:
 
 #16 -> 10
 static func ConverBase16To10Base(arg: String) -> String:
-	return arg;
+	var decimal = 0
+	var length = len(arg) - 1
+	var hexToDec = {
+		'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+		'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15
+	}
+	for i in range(0, len(arg)):
+		var hex = arg.substr(i, 1)
+		decimal += hexToDec[hex] * (16 ** length)
+		length -= 1
+	return str(d
